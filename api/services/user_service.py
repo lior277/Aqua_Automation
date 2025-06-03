6b830f85eb1d96630b8f56d737f7a4b7e95a3989
lior <lior277@gmail.com>
2025-06-03 14:42:24 +0300

    fix

--- api/services/user_service.py
+++ api/services/user_service.py
@@ -15,14 +15,7 @@ class UserService:
         self._id_counter: int = 1
 
     def create_user(self, data: UserCreate) -> User:
-
-        if data.israel_id in self._israeli_id_index:
-            existing_user_id = self._israeli_id_index[data.israel_id]
-            logger.warning(
-                "Duplicate Israeli ID: %s (user_id: %d)",
-                data.israel_id, existing_user_id
-            )
-            raise UserAlreadyExistsError(data.israel_id)
+        self._validate_unique_israeli_id(data.israel_id)
 
         user = User(
             user_id=self._id_counter,
@@ -30,11 +23,22 @@ class UserService:
         )
         self._id_counter += 1
 
-        self._db[user.user_id] = user
-        self._israeli_id_index[user.israel_id] = user.user_id
-        logger.info("Created User: %s", user.__dict__)
-
+        self._save_user(user)
         return user
+
+    def _validate_unique_israeli_id(self, israel_id: str) -> None:
+        if israel_id in self._israeli_id_index:
+            existing_user_id = self._israeli_id_index[israel_id]
+            logger.warning(
+                "Duplicate Israeli ID: %s (user_id: %d)",
+                israel_id, existing_user_id
+            )
+            raise UserAlreadyExistsError(israel_id)
+
+    def _save_user(self, user: User) -> None:
+        self._db[user.user_id] = user
+        self._israeli_id_index[user.israeli_id] = user.user_id
+        logger.info("Created User: %s", user.__dict__)
 
     def get_user(self, user_id: int) -> Optional[User]:
         user = self._db.get(user_id)
@@ -63,13 +67,3 @@ class UserService:
         self._israeli_id_index.clear()
         self._id_counter = 1
         logger.info("Cleared all user data")
-
-
-# Dependency injection setup
-# For in-memory storage, we need a shared instance
-# In production, this should be replaced with a proper database
-_user_service_instance = UserService()
-
-
-def get_user_service() -> UserService:
-    return _user_service_instance
